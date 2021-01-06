30669ebe9bdefb7060cf3f60482ca8282a5df11e
Darth9 <reyprimerjefe@gmail.com>
2021-01-06 12:45:43 +0000

    Remove unnecessary stuff

--- device.mk
+++ device.mk
@@ -1,13 +1,11 @@
 # dynamic partition stuff
 PRODUCT_USE_DYNAMIC_PARTITIONS := true
-
 # Reserve 64MB in dynamic partitions
 #BOARD_SYSTEMIMAGE_PARTITION_RESERVED_SIZE := 67108864
 #BOARD_VENDORIMAGE_PARTITION_RESERVED_SIZE := 67108864
 
 # fastbootd stuff
 PRODUCT_PACKAGES += \
-    fastbootd \
     android.hardware.fastboot@1.0-impl-mock \
     android.hardware.fastboot@1.0-impl-mock.recovery
 #
