ac87f71b3bf3303c8834d78ec7974cf6f248c47c
Darth9 <reyprimerjefe@gmail.com>
2021-01-06 12:42:49 +0000

    Update fastbootd stuff

--- device.mk
+++ device.mk
@@ -1,6 +1,13 @@
 # dynamic partition stuff
 PRODUCT_USE_DYNAMIC_PARTITIONS := true
+
 # Reserve 64MB in dynamic partitions
 #BOARD_SYSTEMIMAGE_PARTITION_RESERVED_SIZE := 67108864
 #BOARD_VENDORIMAGE_PARTITION_RESERVED_SIZE := 67108864
+
+# fastbootd stuff
+PRODUCT_PACKAGES += \
+    fastbootd \
+    android.hardware.fastboot@1.0-impl-mock \
+    android.hardware.fastboot@1.0-impl-mock.recovery
 #
