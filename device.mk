6cc5566f8b6090a31482089504c1291f0bc4909d
Darth9 <reyprimerjefe@gmail.com>
2021-01-06 12:48:04 +0000

    Add copyright notice

--- device.mk
+++ device.mk
@@ -1,5 +1,26 @@
+#
+#	This file is part of the OrangeFox Recovery Project
+# 	Copyright (C) 2020-2021 The OrangeFox Recovery Project
+#
+#	OrangeFox is free software: you can redistribute it and/or modify
+#	it under the terms of the GNU General Public License as published by
+#	the Free Software Foundation, either version 3 of the License, or
+#	any later version.
+#
+#	OrangeFox is distributed in the hope that it will be useful,
+#	but WITHOUT ANY WARRANTY; without even the implied warranty of
+#	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#	GNU General Public License for more details.
+#
+# 	This software is released under GPL version 3 or any later version.
+#	See <http://www.gnu.org/licenses/>.
+#
+# 	Please maintain this if you use this script or any part of it
+#
+
 # dynamic partition stuff
 PRODUCT_USE_DYNAMIC_PARTITIONS := true
+
 # Reserve 64MB in dynamic partitions
 #BOARD_SYSTEMIMAGE_PARTITION_RESERVED_SIZE := 67108864
 #BOARD_VENDORIMAGE_PARTITION_RESERVED_SIZE := 67108864
