0b59ae82b5852f9938690f21e8e398ae0d538092
Ctapchuk <pilipenko0409@gmail.com>
2021-09-16 19:34:08 +0700

    Remove ashmemd It was useless

--- device.mk
+++ device.mk
@@ -39,20 +39,12 @@ PRODUCT_EXTRA_RECOVERY_KEYS += \
 # Take a few libraries from sources
 TARGET_RECOVERY_DEVICE_MODULES += \
     android.hidl.base@1.0 \
-    ashmemd \
-    ashmemd_aidl_interface-cpp \
-    libashmemd_client \
     libicui18n \
     libion \
     libxml2
 
-TW_RECOVERY_ADDITIONAL_RELINK_BINARY_FILES += \
-    $(TARGET_OUT_EXECUTABLES)/ashmemd
-
 TW_RECOVERY_ADDITIONAL_RELINK_LIBRARY_FILES += \
     $(TARGET_OUT_SHARED_LIBRARIES)/android.hidl.base@1.0.so \
-    $(TARGET_OUT_SHARED_LIBRARIES)/ashmemd_aidl_interface-cpp.so \
-    $(TARGET_OUT_SHARED_LIBRARIES)/libashmemd_client.so \
     $(TARGET_OUT_SHARED_LIBRARIES)/libicui18n.so \
     $(TARGET_OUT_SHARED_LIBRARIES)/libion.so \
     $(TARGET_OUT_SHARED_LIBRARIES)/libxml2.so
