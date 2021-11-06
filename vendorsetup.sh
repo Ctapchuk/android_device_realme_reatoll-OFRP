acd690834294b0ac753e26d33519ada1d24e4e0d
Ctapchuk <pilipenko0409@gmail.com>
2021-11-07 02:11:06 +0700

    Remove Magisk [don't merge if using official OFRP sources]

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -48,7 +48,6 @@ if [ "$1" = "$FDEVICE" -o "$FOX_BUILD_DEVICE" = "$FDEVICE" ]; then
 	export OF_DISABLE_MIUI_SPECIFIC_FEATURES=1
 	export FOX_RECOVERY_SYSTEM_PARTITION="/dev/block/mapper/system"
 	export FOX_RECOVERY_VENDOR_PARTITION="/dev/block/mapper/vendor"
-	export FOX_USE_SPECIFIC_MAGISK_ZIP="$PWD/$DEVICE_PATH/magisk/Magisk.zip"
 	export FOX_DELETE_INITD_ADDON=1
 
 	#OFR binary files
