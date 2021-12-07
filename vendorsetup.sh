d703cb3d8c6f39eeecba6acfb1d868b11c8fe334
Ctapchuk <pilipenko0409@gmail.com>
2021-12-07 19:57:57 +0700

    Add phhusson's LPTOOLS to the build

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -59,6 +59,7 @@ if [ "$1" = "$FDEVICE" -o "$FOX_BUILD_DEVICE" = "$FDEVICE" ]; then
 	export FOX_USE_TAR_BINARY=1
 	export FOX_USE_XZ_UTILS=1
 	export FOX_USE_SED_BINARY=1
+	export OF_ENABLE_LPTOOLS=1
 	
 	#OFR patches
 	export OF_USE_MAGISKBOOT_FOR_ALL_PATCHES=1
