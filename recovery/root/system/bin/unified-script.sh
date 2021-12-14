a75ea14c9f594465f3467520890b7f7746c7111e
Ctapchuk <pilipenko0409@gmail.com>
2021-12-14 18:16:05 +0700

    Set "ro.separate.soft" prop for realmeUI OTA installation Fix realmeUI 2.0 OTA installation on realme 7 Pro

--- recovery/root/system/bin/unified-script.sh
+++ recovery/root/system/bin/unified-script.sh
@@ -10,6 +10,7 @@ load_RMX2061()
     resetprop "ro.product.device" "RMX2061"
     resetprop "ro.product.product.device" "RMX2061"
     resetprop "ro.vendor.product.device" "RMX2061"
+    resetprop "ro.separate.soft" "19721"
     echo "realme 6 Pro detected - using RMX2061 props"
 }
 
@@ -21,6 +22,7 @@ load_RMX2170()
     resetprop "ro.product.device" "RMX2170"
     resetprop "ro.product.product.device" "RMX2170"
     resetprop "ro.vendor.product.device" "RMX2170"
+    resetprop "ro.separate.soft" "206B1"
     echo "realme 7 Pro detected - using RMX2170 props"
 }
 
