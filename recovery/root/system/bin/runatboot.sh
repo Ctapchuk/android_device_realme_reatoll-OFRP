ed862bc5b5cefb5647855d39a108bd961f8f4dde
Egor Pilipenko <pilipenko0409@gmail.com>
2022-08-31 13:23:11 +0700

    Update unified-script

--- recovery/root/system/bin/runatboot.sh
+++ recovery/root/system/bin/runatboot.sh
@@ -1,34 +1,34 @@
 #!/system/bin/sh
 
-# This script is needed to automatically set device props.
+# This script is needed to automatically set props for unified devices.
 
 load_RMX2061()
 {
-    resetprop "ro.product.model" "realme 6 Pro"
-    resetprop "ro.product.name" "RMX2061"
-    resetprop "ro.build.product" "RMX2061"
-    resetprop "ro.product.device" "RMX2061"
-    resetprop "ro.product.product.device" "RMX2061"
-    resetprop "ro.vendor.product.device" "RMX2061"
-    resetprop "ro.separate.soft" "19721"
-    echo "realme 6 Pro detected - using RMX2061 props"
+    resetprop "ro.product.model" "realme 6 Pro";
+    resetprop "ro.product.name" "RMX2061";
+    resetprop "ro.build.product" "RMX2061";
+    resetprop "ro.product.device" "RMX2061";
+    resetprop "ro.product.product.device" "RMX2061";
+    resetprop "ro.vendor.product.device" "RMX2061";
+    echo "I:unified-script: realme 6 Pro detected - using RMX2061 props" >> $LOGF;
 }
 
 load_RMX2170()
 {
-    resetprop "ro.product.model" "realme 7 Pro"
-    resetprop "ro.product.name" "RMX2170"
-    resetprop "ro.build.product" "RMX2170"
-    resetprop "ro.product.device" "RMX2170"
-    resetprop "ro.product.product.device" "RMX2170"
-    resetprop "ro.vendor.product.device" "RMX2170"
-    resetprop "ro.separate.soft" "206B1"
-    echo "realme 7 Pro detected - using RMX2170 props"
+    resetprop "ro.product.model" "realme 7 Pro";
+    resetprop "ro.product.name" "RMX2170";
+    resetprop "ro.build.product" "RMX2170";
+    resetprop "ro.product.device" "RMX2170";
+    resetprop "ro.product.product.device" "RMX2170";
+    resetprop "ro.vendor.product.device" "RMX2170";
+    echo "I:unified-script: realme 7 Pro detected - using RMX2170 props" >> $LOGF;
 }
 
+local LOGF=/tmp/recovery.log;
+project=$(cat /proc/oppoVersion/prjName);
 
-project=$(cat /proc/oppoVersion/prjName)
-echo $project
+echo "I:unified-script: oppo project name:" $project >> $LOGF;
+resetprop "ro.separate.soft" $project;
 
 case $project in
     "206B1")
@@ -38,7 +38,7 @@ case $project in
         load_RMX2061
         ;;
      *)
-        echo "Invalid device code name"
+        echo "E:unified-script: unsupported project!" $project >> $LOGF;
         ;;
 esac
 
