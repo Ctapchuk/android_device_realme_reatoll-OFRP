e2d2dcae6ecf9548a28b6d188d1f695710b981b4
Egor Pilipenko <pilipenko0409@gmail.com>
2023-09-06 01:33:08 +0700

    Add UI props to gui-props script -Drop OFRP vendor vars

--- recovery/root/system/bin/gui-props.sh
+++ recovery/root/system/bin/gui-props.sh
@@ -1,16 +1,25 @@
 #!/system/bin/sh
 
 # This script is needed to automatically set GUI props for unified devices.
+load_reatoll()
+{
+    setprop "fox.status.right.offset" "34";
+    setprop "fox.screen.height" "2400";
+}
 
 load_RMX2061()
 {
     setprop "fox.status.left.offset" "261";
+    setprop "fox.status.height.offset" "108";
+    setprop "fox.status.placement.offset" "38";
     echo "I:GUI-props: using props for" $project >> $LOGF;
 }
 
 load_RMX3081_2170()
 {
-    setprop "fox.status.left.offset" "181";
+    setprop "fox.status.left.offset" "158";
+    setprop "fox.status.height.offset" "140";
+    setprop "fox.status.placement.offset" "40";
     echo "I:GUI-props: using props for" $project >> $LOGF;
 }
 
@@ -28,5 +37,7 @@ case $project in
         echo "E:GUI-props: unsupported project:" $project >> $LOGF;
         ;;
 esac
+
+load_reatoll
 
 exit 0
